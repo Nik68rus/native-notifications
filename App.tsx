873c949fbe8443755c8ecb97550667163c68997a
Nik68rus <nik200x@mail.ru>
2023-03-10 20:48:03 +0300

    notifications example

--- App.tsx
+++ App.tsx
@@ -1,10 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, View, Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { useEffect, useState } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    const requestPermissionsAsync = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (finalStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== 'granted') {
+        Alert.alert(
+          'Permission required!',
+          'Push notifications need permissions!'
+        );
+
+        return;
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      setToken(pushTokenData.data);
+
+      if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    };
+    requestPermissionsAsync();
+  }, []);
+
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('notif recieved');
+        console.log(notification);
+        const user = notification.request.content.data.userName;
+        console.log(user);
+      }
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log('notif resp recieved');
+        console.log(response);
+        const user = response.notification.request.content.data.userName;
+        console.log(user);
+      }
+    );
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Local notification',
+        body: 'Body of notification',
+        data: { userName: 'Nik' },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
+
+  const sendPushHandler = () => {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: token,
+        title: 'Test frim device',
+        body: 'This is test again',
+      }),
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Schedule notification"
+        onPress={scheduleNotificationHandler}
+      />
+      <Button title="Send push" onPress={sendPushHandler} />
       <StatusBar style="auto" />
     </View>
   );
